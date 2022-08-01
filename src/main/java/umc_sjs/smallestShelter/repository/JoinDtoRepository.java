b174d5d7a3bd702d0c61f2bb98c48ba5a3462509
myeondii <khkkgon0118@gmail.com>
2022-08-01 20:08:39 +0900

    Delete JoinDtoRepository.java

--- src/main/java/umc_sjs/smallestShelter/repository/JoinDtoRepository.java
+++
@@ -1,9 +0,0 @@
-package umc_sjs.smallestShelter.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import umc_sjs.smallestShelter.dto.JoinDto;
-
-public interface JoinDtoRepository extends JpaRepository<JoinDto, Long> {
-    JoinDto findByUserName(String username);
-
-}
