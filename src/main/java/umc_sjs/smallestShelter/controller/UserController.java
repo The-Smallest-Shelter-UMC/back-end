4387a7ea8ec05bc3a564da8d2a444af67c02f6b7
kyj <khkkgon0118@gmail.com>
2022-08-03 15:27:35 +0900

    마이페이지 - 개인정보 API (PRIVATE)

---
+++ src/main/java/umc_sjs/smallestShelter/controller/UserController.java
@@ -0,0 +1,45 @@
+package umc_sjs.smallestShelter.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+import umc_sjs.smallestShelter.config.auth.PrincipalDetails;
+import umc_sjs.smallestShelter.dto.user.GetPrivatePageRes;
+import umc_sjs.smallestShelter.dto.user.JoinDto;
+import umc_sjs.smallestShelter.service.UserService;
+
+import java.io.IOException;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/join")
+    public String join(@RequestBody JoinDto joinDto) throws IOException {
+        userService.join(joinDto);
+        return "회원가입 완료";
+    }
+
+    @GetMapping("/auth/private/{userIdx}") // 마이페이지 - 개인
+    public GetPrivatePageRes privatePage(@PathVariable Long userIdx, Authentication authentication) throws IOException {
+        PrincipalDetails principalDetails = (PrincipalDetails) authentication.getPrincipal();
+
+        System.out.println("UserController**********************" + principalDetails.getUsername());
+
+        System.out.println("UserController**********************");
+
+        if (principalDetails.getUser().getIdx() == userIdx) {
+            return userService.privatePage(userIdx);
+        } else {
+            return null;
+        }
+    }
+
+//    @GetMapping("/auth/organization/{userIdx}") // 마이페이지 - 단체
+//    public String organizationPage(@PathVariable Long userIdx) throws IOException {
+//        userService.organizationPage(userIdx);
+//        return "회원가입 완료";
+//    }
+}
