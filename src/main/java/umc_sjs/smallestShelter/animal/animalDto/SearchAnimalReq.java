0b9f893d696bb2e03afb5afc805762232cc6827b
KeonhoPark <gse04063@gmail.com>
2022-08-12 19:49:16 +0900

    searchAnimalReq 수정

--- src/main/java/umc_sjs/smallestShelter/animal/animalDto/SearchAnimalReq.java
+++ src/main/java/umc_sjs/smallestShelter/animal/animalDto/SearchAnimalReq.java
@@ -6,12 +6,18 @@ import umc_sjs.smallestShelter.domain.AgeBoundary;
 import umc_sjs.smallestShelter.domain.Gender;
 import umc_sjs.smallestShelter.domain.Species;
 
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+
 @Getter
 @Setter
 public class SearchAnimalReq {
 
+    @Enumerated(EnumType.STRING)
     private Species species;
+    @Enumerated(EnumType.STRING)
     private Gender gender;
+    @Enumerated(EnumType.STRING)
     private AgeBoundary ageBoundary;
     private Boolean isAdopted;
 }
